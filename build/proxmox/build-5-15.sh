d31de448a02bcf760426f97a68c40a87e443572b
EuNeko <61500832+EuNeko@users.noreply.github.com>
2023-03-09 12:55:16 +0400

    Create build-5-15.sh

---
+++ build/proxmox/build-5-15.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+export PVE_KERNEL_BRANCH=pve-kernel-5.15
+export RELAX_INTEL_GIT_REPO="https://github.com/EuNeko/relax-intel-rmrr.git"
+export RELAX_PATCH="add-relaxable-rmrr-5_15.patch"
+export PROXMOX_PATCH="proxmox.patch"
+
+./build.sh
